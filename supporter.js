a005e12012f92c1f4856dc6bd4d794ee4060c4a2
normande <normande@gmail.com>
2013-07-25 02:25:52 +0000

    add needs

--- supporter.js
+++ supporter.js
@@ -10,10 +10,39 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+  
+   Template.newNeed.events({
+  'click input#newNeedButton' : function () {
+    // user is creating a resource
+    addNeed();
+   }
+  });
+  
+  Template.newNeed.events({
+  'keypress input#newNeed' : function (evt) {
+     // user is creating a resource
+     if (evt.which === 13) {
+      addNeed();
+     }
+   }
+  });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function addNeed() {
+  var user = Meteor.user();
+    if (user && user.username) {
+      var newNeed = document.getElementById("newNeed").value;
+      Needs.insert({
+      content : newNeed, 
+      createdAt: new Date(),
+      userId : user._Id, 
+      username : user.username});
+    }
 }
