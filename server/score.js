8cd70a434641ddac93352c8ebea99f7dbb824fe5
Ry Walker <ry@rywalker.com>
2013-12-15 10:01:13 -0500

    Whitespace

--- server/score.js
+++ server/score.js
@@ -4,15 +4,12 @@ Meteor.methods({
 
   newScore = Supporter.computeScore(need)
 
-  // Note: before the first time updateScore runs on a new need, its score will be at 0
   var scoreDiff = Math.abs(need.score - newScore);
 
-  // only update database if difference is larger than x to avoid unnecessary updates
   if (forceUpdate || scoreDiff > x) {
     Needs.update(need._id, {$set: {score: newScore, inactive: false}});
     return 1;
-  }else if(ageInHours > n*24){
-    // only set a post as inactive if it's older than n days
+  } else if(ageInHours > n*24) {
     Needs.update(need._id, {$set: {inactive: true}});
   }
   return 0;
